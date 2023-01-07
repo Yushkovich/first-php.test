3ec247c993e6dc2eb0b2634b936cb5591c3f081d
“Vitali <vitali_Shulha@epam.comgit config --global user.email vitali_Shulha@epam.com>
2023-01-07 04:10:20 +0200

    SOlved

--- partials/header.php
+++ partials/header.php
@@ -1,7 +1,30 @@
-<?php
-    require($_SERVER['DOCUMENT_ROOT'] . '/configs/db.php');
 
-    session_start();
+<?php
+require($_SERVER['DOCUMENT_ROOT'] . '/configs/db.php');
+
+session_start();
+
+// проверяем что мы используем сессии
+$is_session = isset($_SESSION["user_id"]) && $_SESSION["user_id"] != null; // true/ false
+// проверяем что мы используем cookie
+$is_cookie = isset($_COOKIE["user_id"]) && $_COOKIE["user_id"] != null; // true/ false
+
+// если пользователь не авторизован - то на главную страницу
+if($is_session || $is_cookie) {
+// выбираем пользователя
+    $userID = $is_session ? $_SESSION["user_id"] : $_COOKIE["user_id"]; //тринарный оператор
+    $sql = "SELECT * FROM users WHERE id =" . $userID; 
+     
+    $result = mysqli_query($conn, $sql);
+    $user = $result->fetch_assoc(); //сохраняемв в юзере нашего пользователя
+      
+// если пользователь role не админ, то делаем редирект на другую страницу
+       if($user['role'] != "admin"){
+           header("Location: /login.php");
+        }
+    } else { //если вообще нет авторизации тоже редирект
+            header("Location: /login.php");
+    }
 ?>
 
 
@@ -35,7 +58,7 @@
 <?php
     if(isset($_SESSION["user_id"]) && $_SESSION["user_id"] != null) {
     echo '<a href="logout.php">Logout</a>';
-    } else if if(isset($_COOKIE["user_id"]) && $_COOKIE["user_id"] != null) {
+    } else if(isset($_COOKIE["user_id"]) && $_COOKIE["user_id"] != null) {
       echo '<a href="logout.php">Logout</a>';
       } else {
 ?>
